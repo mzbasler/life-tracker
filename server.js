37eafa1e1437480a20af8c181359860667985c6f
Julia Perini <143840382+juliafperini@users.noreply.github.com>
2023-11-16 12:46:28 +0000

    ajustes rotas e caminhos de arquivos

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const path = require('path');
 const server = http.createServer((req, res) => {
   let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
 
-  if(req.url === 'dashboard.html'){
-    filePath = path.join(__dirname, req.url === '/cadastrar.html' ? '/cadastrar.html' : req.url);
+  if(req.url === '/cadastrar.html'){
+    filePath = path.join(__dirname, req.url === '/cadastrar.html' ? '/assets/pages/cadastrar.html' : req.url);
   }
-  if(req.url === 'login.html'){
-    filePath = path.join(__dirname, req.url === '/login.html' ? '/login.html' : req.url);
+  if(req.url === '/login.html'){
+    filePath = path.join(__dirname, req.url === '/login.html' ? '/assets/pages/login.html' : req.url);
+  }
+  if(req.url === '/dashboard.html'){
+    filePath = path.join(__dirname, req.url === '/dashboard.html' ? '/assets/pages/dashboard.html' : req.url);
   }
 
   fs.readFile(filePath, (err, data) => {
