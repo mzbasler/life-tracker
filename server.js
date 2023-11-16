49e9415c8596d4d65091fe7e97cd06e80ff3485b
Julia Perini <143840382+juliafperini@users.noreply.github.com>
2023-11-16 12:23:54 +0000

    pagina cadastrar pronta

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-  let filePath = path.join(__dirname, req.url === '/' ? '/assets/pages/login.html' : req.url);
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
 
   if(req.url === 'dashboard.html'){
-    filePath = path.join(__dirname, req.url === '/dashboard.html' ? '/assets/pages/dashboard.html' : req.url);
+    filePath = path.join(__dirname, req.url === '/cadastrar.html' ? '/cadastrar.html' : req.url);
   }
   if(req.url === 'login.html'){
-    filePath = path.join(__dirname, req.url === '/login.html' ? '/assets/pages/login.html' : req.url);
+    filePath = path.join(__dirname, req.url === '/login.html' ? '/login.html' : req.url);
   }
 
   fs.readFile(filePath, (err, data) => {
