ace9bbde48bcb73bbf01f2cd39897a8d85463287
Maurício Basler <basler.mauricio@gmail.com>
2023-11-17 20:18:03 -0300

    alteração dos caminhos das rotas nos arquivos login.js e cadastrar.js

--- assets/js/cadastrar.js
+++ assets/js/cadastrar.js
@@ -1,32 +1,30 @@
 function verificarSenha() {
-    var senha = $('#senha').val();
-    var repetirSenha = $('#repetir-senha').val();
-  
-    if (senha !== repetirSenha) {
-      alert('A senha não coincide com a repetição da senha. Tente novamente.');
-      return false;
-    }
-  
-    return true;
-  }
-  
-  $('#cadastrarButton').on('click', function () {
-    if (verificarSenha()) {
-      var email = $('#email').val();
-      var senha = $('#senha').val();
-  
-      // Obtenha os dados armazenados
-      var dadosArmazenados = JSON.parse(localStorage.getItem('usuarios')) || [];
-  
-      // Adicione o novo usuário aos dados armazenados
-      dadosArmazenados.push({ email: email, senha: senha });
-  
-      // Atualize o localStorage
-      localStorage.setItem('usuarios', JSON.stringify(dadosArmazenados));
-  
-      alert('Usuário cadastrado com sucesso');
-      window.location.href = '/login.html';
-    }
+  var senha = $("#senha").val();
+  var repetirSenha = $("#repetir-senha").val();
 
+  if (senha !== repetirSenha) {
+    alert("A senha não coincide com a repetição da senha. Tente novamente.");
+    return false;
+  }
+
+  return true;
+}
+
+$("#cadastrarButton").on("click", function () {
+  if (verificarSenha()) {
+    var email = $("#email").val();
+    var senha = $("#senha").val();
+
+    // Obtenha os dados armazenados
+    var dadosArmazenados = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+    // Adicione o novo usuário aos dados armazenados
+    dadosArmazenados.push({ email: email, senha: senha });
+
+    // Atualize o localStorage
+    localStorage.setItem("usuarios", JSON.stringify(dadosArmazenados));
+
+    alert("Usuário cadastrado com sucesso");
+    window.location.href = "/assets/pages/login.html";
+  }
 });
-  
